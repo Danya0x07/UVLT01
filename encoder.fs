a7c2bb7a12d91346d33f065c0d867b5dfe9a6bb0
Danya0x07 <dlef0xf8@gmail.com>
2021-07-30 00:40:53 +0300

    Replaced tabs with spaces.

--- encoder.fs
+++ encoder.fs
@@ -29,73 +29,73 @@ variable _enc.curr
 variable _?btn.prev
 
 : encoder-init  ( -- )
-	\ Ininialize GPIOs of encoder and its button.
-	[ 1 PC_CR1 ENCODER_BTN ]B!  \ internal pullup on
-	[ $9B C, ]    \ ASM("SIM") - disable interrupts
-	$30 EXTI_CR1 C!  \ PortC interrupt on
-	[ $9A C, ] ;  \ ASM("RIM") - enable interrupts
+    \ Ininialize GPIOs of encoder and its button.
+    [ 1 PC_CR1 ENCODER_BTN ]B!  \ internal pullup on
+    [ $9B C, ]    \ ASM("SIM") - disable interrupts
+    $30 EXTI_CR1 C!  \ PortC interrupt on
+    [ $9A C, ] ;  \ ASM("RIM") - enable interrupts
 
 : encoder-irq!  ( 1|0 -- )
-	\ Manage encoder interrupt
-	PC_CR2 ENCODER_CLK B! ;
+    \ Manage encoder interrupt
+    PC_CR2 ENCODER_CLK B! ;
 
 \ Relationship between encoder pin statuses and its steps
 \ looks like that: (high: -1, low: 0)
 \
-\			CLK
-\         /     \ 
-\		 -1		 0
-\		 DT		DT
+\           CLK
+\         /     \
+\        -1      0
+\        DT     DT
 \       /  \   /  \
-\	   -1   0 -1   0
+\      -1   0 -1   0
 \      |    | |    |
 \      -1  +1 +1   -1
 \
 \ So the formula can be: -(CLK xor DT), if 0 then -1 .
 
 : _enc.calc-step  ( CLK DT -- step)
-	\ Calculate encoder step (+1 | -1) from its pins statuses
-	xor negate 
-	dup 0= if drop -1 then ;
+    \ Calculate encoder step (+1 | -1) from its pins statuses
+    xor negate
+    dup 0= if drop -1 then ;
 
 : _enc.constrain  ( updated -- constrained)
-	\ Constrain encoder current value according to bounds.
-	dup _enc.max @ > if drop _enc.max @ then
-	dup _enc.min @ < if drop _enc.min @ then ;
+    \ Constrain encoder current value according to bounds.
+    dup _enc.max @ > if drop _enc.max @ then
+    dup _enc.min @ < if drop _enc.min @ then ;
 
 :NVM  \ Encoder ISR.
-	SAVEC
-	0 encoder-irq!
-	
-	[ PC_IDR ENCODER_CLK ]B?  \ get CLK
-	[ PC_IDR ENCODER_DT ]B?   \ get DT
-	_enc.calc-step 	_enc.curr @  +  \ this will be new encoder value
-	\ Check it and then write.
-	_enc.constrain	_enc.curr !
-	
-	1 encoder-irq!
-	IRET
+    SAVEC
+    0 encoder-irq!
+
+    [ PC_IDR ENCODER_CLK ]B?  \ get CLK
+    [ PC_IDR ENCODER_DT ]B?   \ get DT
+    _enc.calc-step  _enc.curr @  +  \ this will be new encoder value
+    \ Check it and then write.
+    _enc.constrain  _enc.curr !
+
+    1 encoder-irq!
+    IRET
 ;NVM INT_EXTI2 !
 
 : encoder-set  ( min max curr -- )
-	\ Set encoder bounds and current value.
-	_enc.curr !  _enc.max !  _enc.min ! ;
+    \ Set encoder bounds and current value.
+    _enc.curr !  _enc.max !  _enc.min ! ;
 
 : encoder-get  ( -- curr )
-	\ Get current encoder value.
-	_enc.curr @ ;
+    \ Get current encoder value.
+    _enc.curr @ ;
 
 : button-is-down?  ( -- ? )
-	\ Check if button is being pressed now.
-	[ PC_IDR ENCODER_BTN ]B? not ;
+    \ Check if button is being pressed now.
+    [ PC_IDR ENCODER_BTN ]B? not ;
 
 : button-pressed?  ( -- ? )
-	\ Check if there was button pressed after it was released.
-	_?btn.prev @  button-is-down?  2dup <> if
-		5 ms-wait
-		drop
-		button-is-down?
-	then  ( prev new )
+    \ Check if there was button pressed after it was released.
+    _?btn.prev @  button-is-down?  2dup <> if
+            5 ms-wait
+            drop
+            button-is-down? then
+    ( prev new )
     dup rot not and  ( new ? )
     swap _?btn.prev ! ;
 
--- time.fs
+++ time.fs
@@ -16,35 +16,35 @@ NVM
 variable millis
 
 :NVM
-	SAVEC
-	1 millis +!
-	[ 0 TIM4_SR 0 ]B!	\ clear interrupt pending bit
-	IRET
+    SAVEC
+    1 millis +!
+    [ 0 TIM4_SR 0 ]B!   \ clear interrupt pending bit
+    IRET
 ;NVM INT_TIM4 !
 
 : time-init  ( -- )
-	7 TIM4_PSCR C!   \ presc. 128
-	124 TIM4_ARR C!  \ period 125
-	[ 0 TIM4_SR 0 ]B!	\ clear flag
-	[ 1 TIM4_IER 0 ]B! 	\ and enable interrupt
-	;
+    7 TIM4_PSCR C!   \ presc. 128
+    124 TIM4_ARR C!  \ period 125
+    [ 0 TIM4_SR 0 ]B!   \ clear flag
+    [ 1 TIM4_IER 0 ]B!  \ and enable interrupt
+    ;
 
 : time-start  ( -- )
-	\ Start counting milliseconds from 0.
-	0 millis !
-	[ 1 TIM4_CR1 0 ]B! ;
+    \ Start counting milliseconds from 0.
+    0 millis !
+    [ 1 TIM4_CR1 0 ]B! ;
 
 : time-stop  ( -- )
-	\ Stop counting milliseconds.
-	[ 0 TIM4_CR1 0 ]B! ;
+    \ Stop counting milliseconds.
+    [ 0 TIM4_CR1 0 ]B! ;
 
 : ms-passed?  ( ms -- ? )
-	\ Indicates that at least <ms> have passed.
-	millis @ > not ;
+    \ Indicates that at least <ms> have passed.
+    millis @ > not ;
 
 : ms-wait  ( ms -- )
-	time-start
-	begin dup ms-passed? until
-	drop time-stop ;
+    time-start
+    begin dup ms-passed? until
+    drop time-stop ;
 
 RAM
